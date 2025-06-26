edbfb2ce7510ecb0f48598f87194381d092b8fb7
ethanrgibson <ethanrgibson33@gmail.com>
2025-06-26 11:23:55 -0600

    added more colors

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
+const classes = ['red', 'color', 'blue', 'black', 'goldenrod', 'green', 'pink']
+let index = -1
 function changeColor() {
+  index++
+  if (index == classes.length) index = 0
   let word = document.getElementById('magic')
-  word.classList.toggle('exciting')
+  for (let i = 0; i < classes.length; i++) {
+    const classColor = classes[i];
+    if (index == i) {
+      word.classList.add(classColor)
+    }
+    else {
+      word.classList.remove(classColor)
+    }
+  }
 }
-setInterval(changeColor, 1000)
+setInterval(changeColor, 500)
