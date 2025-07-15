989b6dc6dc7570f6e10a0ca3b832d03d6b909c8c
ethanrgibson <ethanrgibson33@gmail.com>
2025-07-15 14:40:13 -0600

    extra section made

--- app.js
+++ app.js
@@ -7,40 +7,17 @@ function changeColor() {
   index++
   if (index == classes.length) index = 0
   let word = document.getElementById('magic')
+  let word2 = document.getElementById('extracurricular')
   for (let i = 0; i < classes.length; i++) {
     const classColor = classes[i];
     if (index == i) {
       word.classList.add(classColor)
+      word2.classList.add(classColor)
     }
     else {
       word.classList.remove(classColor)
+      word2.classList.remove(classColor)
     }
   }
 }
 setInterval(changeColor, 750)
-
-
-
-// Service Scroller
-
-// const scrollers = document.querySelectorAll('.scroller')
-
-// if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-//   addAnimation()
-// }
-
-// function addAnimation() {
-//   scrollers.forEach((scroller) => {
-//     scroller.setAttribute('data-animated', 'true')
-
-//     const scrollerInner = scroller.querySelector('.scroller-inner')
-//     const scrollerContent = Array.from(scrollerInner.children)
-
-//     scrollerContent.forEach((item) => {
-//       const duplicatedItem = item.cloneNode(true)
-//       scrollerInner.appendChild(duplicatedItem)
-//     })
-//   });
-
-
-// }
